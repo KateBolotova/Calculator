b1e328755a899b5dd8dff2834661ccd10ba25dd9
Ekaterina Bolotova <kate.bolotova2003@gmail.com>
2023-10-07 13:03:57 +0300

    Added test file for TDD, function in calculator, test for empty matrix in working

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def matrix_det(matrix_count_det):
+    a = matrix_count_det[0][0]
+    b = matrix_count_det[0][1]
+    c = matrix_count_det[0][2]
+    d = matrix_count_det[1][0]
+    e = matrix_count_det[1][1]
+    f = matrix_count_det[1][2]
+    g = matrix_count_det[2][0]
+    h = matrix_count_det[2][1]
+    i = matrix_count_det[2][2]
+
+    det = a * (e * i - f * h) - b * (d * i - f * g) + c * (d * h - e * g)
+
+    return det
