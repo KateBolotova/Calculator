b934eb7791751de3f6b187a4d4c6b6ac4184230a
Ekaterina Bolotova <kate.bolotova2003@gmail.com>
2023-10-07 13:30:00 +0300

    Reduced the reading of the matrix

--- calculator.py
+++ calculator.py
@@ -1,21 +1,16 @@
 def matrix_det(matrix_count_det):
-
     # Проверка типов элементов матрицы
     for row in matrix_count_det:
         for element in row:
             if not isinstance(element, (int, float)):
                 raise ValueError("Матрица должна содержать только числа")
 
-    a = matrix_count_det[0][0]
-    b = matrix_count_det[0][1]
-    c = matrix_count_det[0][2]
-    d = matrix_count_det[1][0]
-    e = matrix_count_det[1][1]
-    f = matrix_count_det[1][2]
-    g = matrix_count_det[2][0]
-    h = matrix_count_det[2][1]
-    i = matrix_count_det[2][2]
+    if not matrix_count_det:  # Если матрица пустая, возвращаем определитель равный нулю
+        return 0
 
-    det = a * (e * i - f * h) - b * (d * i - f * g) + c * (d * h - e * g)
+    a, b, c = matrix_count_det[0]
+    d, e, f = matrix_count_det[1]
+    g, h, i = matrix_count_det[2]
 
+    det = a * e * i + b * f * g + c * d * h - c * e * g - b * d * i - a * f * h
     return det
