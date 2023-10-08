d9221b98d880c62e9ba700a8c9ee467f4cae23bb
Evgeniya Mishchenko <evgeniyalike@yandex.ru>
2023-10-08 22:18:48 +0300

    Тест 5 test_0_and_1() 2 Failed

--- test_fac.py
+++ test_fac.py
@@ -8,3 +8,8 @@ def test_digit():
 def test_empty():
     with pytest.raises(TypeError):
         fac()
+
+@pytest.mark.parametrize('num, expect', [(0, 1), (1, 1)])
+
+def test_0_and_1(num, expect):
+    assert fac(num) == expect
