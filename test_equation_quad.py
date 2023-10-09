780c6f9486530cdcc9951b9d2769b28c26f66d51
Zhdanova Ksenia <sovasonya2014@gmail.com>
2023-10-09 17:29:03 +0300

    Simple test is passed

--- test_equation_quad.py
+++ test_equation_quad.py
@@ -5,7 +5,7 @@ from calculator import equation_quad
 @pytest.mark.parametrize('coeff,roots', [
     ([1, -3, 2], [2, 1]),
     ([1, -15, 14], [14, 1]),
-    ([1, -7, 12], [3, 4])
+    ([1, -7, 12], [4, 3])
 ])
 def test_simple_quad(coeff, roots):
     assert equation_quad(coeff) == roots
