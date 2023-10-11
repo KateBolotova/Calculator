9f0040889314edd334d76162b92e6aa5ef377c1f
Zhdanova Ksenia <sovasonya2014@gmail.com>
2023-10-11 11:46:45 +0300

    Added new test for advanced situations and Adjusted scenario

--- equation_system/scenarios/test_equation_system.py
+++ equation_system/scenarios/test_equation_system.py
@@ -18,6 +18,18 @@ def test_system_2eq():
 def test_system_1eq():
     pass
 
+@scenario('../equation_system.feature','Вычисление корней системы уравнений, когда в одном уравнении есть буквенный символ')
+def test_system_a():
+    pass
+
+@scenario('../equation_system.feature','Вычисление корней системы уравнений без решения')
+def test_system_no_solution():
+    pass
+
+@scenario('../equation_system.feature','Вычисление корней системы уравнений с множеством решений')
+def test_system_many_solutions():
+    pass
+
 @given(parsers.parse('Левая часть уравнений {eq_string}'), target_fixture='left_part')
 def left_part(eq_string):
     equation_matrix = eval(eq_string)
@@ -34,6 +46,19 @@ def right_part(eq_string):
 def solve_eq(left_part, right_part):
     roots = equation_system(left_part, right_part)
     return roots
+
+@when('Я пытаюсь найти решение этого уравнения и ожидаю ошибку', target_fixture='equation_error')
+def solve_eq_error(left_part, right_part):
+    try:
+        equation_system(left_part, right_part)
+    except Exception as e:
+        return e
+
+
+@then(parsers.parse('Исключение должно быть {error_type_string}'))
+def check_result(equation_error, error_type_string):
+    error_type = eval(error_type_string)
+    assert isinstance(equation_error, error_type)
 
 @then(parsers.parse('Результат должен быть {roots_str}'))
 def check_result(found_roots, roots_str):
